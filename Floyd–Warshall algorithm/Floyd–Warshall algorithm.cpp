99e487e537cbcca4ca8126d1a8b02863573d79b0
Caed <Oskar.dyla1999@gmail.com>
2022-01-12 22:54:02 +0100

    Alghorytm done

--- Floyd–Warshall algorithm/Floyd–Warshall algorithm.cpp
+++ Floyd–Warshall algorithm/Floyd–Warshall algorithm.cpp
@@ -6,10 +6,27 @@
 
 using namespace std;
 
+int M(int a, int b)
+{
+    if (a == 0 && b == 0)
+        return 0;
+    else
+        return 1;
+}
+
+int XR(int a, int b)
+{
+    if (a == 1 && b == 1)
+        return 1;
+    else
+        return 0;
+}
+
 int main()
 {
     int Size = 4;
-    int* graph = new int[Size*Size];//remember element ixj is graph[j*Size + i]
+    int* graph = new int[Size * Size];//remember element ixj is graph[j*Size + i]
+    int* dist = new int[Size*Size];
     string el = "";
 
     srand(time(NULL));
@@ -26,7 +43,9 @@ int main()
             cin >> el;
             Size = atoi(el.c_str());
             delete[] graph;
+            delete[] dist;
             graph = new int[Size*Size];
+            dist = new int[Size * Size];
         }
     }
 
@@ -61,7 +80,7 @@ int main()
                 if (rand() % 2 == 0)
                     graph[j * Size + i] = 1;
                 else
-                    graph[j * Size + i] = 0;
+                    graph[j * Size + i] = -1;
             }
         }
     }
@@ -80,7 +99,7 @@ int main()
     {
         for (int j = 0; j < Size; j++)
         {
-
+            dist[j * Size + i] = graph[j * Size + i];
         }
     }
 
@@ -90,12 +109,22 @@ int main()
         {
             for (int j = 0; j < Size; j++)
             {
-
+                dist[j * Size + i] = M(dist[j * Size + i], XR(dist[k * Size + i], dist[i * Size + k]));
             }
         }
     }
 
+    for (int i = 0; i < Size; i++)
+    {
+        for (int j = 0; j < Size; j++)
+        {
+            cout << dist[j * Size + i];
+        }
+        cout << endl;
+    }
+
     delete[] graph;
+    delete[] dist;
 
     return 0;
 }
